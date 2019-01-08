d5ce7437f20e9f7940e696ff2e2300bde42f9559
Jason Miller <jmiller@red-abstract.com>
2019-01-08 06:56:01 -0600

    Adds a required site_secret variable to lock down S3 website with CFront

--- main.tf
+++ main.tf
@@ -21,7 +21,14 @@ resource "aws_s3_bucket" "main_site" {
       ],
       "Effect": "Allow",
       "Resource": "arn:aws:s3:::${var.site_tld}/*",
-      "Principal": {"AWS":"*"}
+      "Principal": {
+          "AWS":"*"
+        }
+      "Condition": {
+        "StringEquals": {
+          "aws:UserAgent": "${var.site_secret}"
+        }
+      }
     }
   ]
 }
@@ -132,9 +139,9 @@ EOF
 # TODO: Add logging to S3 bucket
 
 # CloudFront distribution
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "Origin access identity for ${var.site_tld}"
-}
+# resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+#   comment = "Origin access identity for ${var.site_tld}"
+# }
 
 resource "aws_cloudfront_distribution" "site_cloudfront_distribution" {
   origin {
@@ -148,9 +155,10 @@ resource "aws_cloudfront_distribution" "site_cloudfront_distribution" {
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
     
-    # s3_origin_config {
-    #     origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
-    # }
+    custom_header {
+      name  = "User-Agent"
+      value = "${var.site_secret}"
+    }
   }
 
   enabled = true
