1caee38e88b0001fdd7c3d1cbca39a78a0434dae
Jason Miller <jmiller@red-abstract.com>
2019-04-09 09:39:31 -0700

    CloudFront does not support resource-level perms

--- main.tf
+++ main.tf
@@ -14,15 +14,16 @@ terraform {
 }
 
 locals {
-    site_codecommit_repo_name = "${var.codecommit_repo_name != "" ? var.codecommit_repo_name : var.site_tld}"
-    site_tld_shortname = "${replace(var.site_tld, ".", "")}"
+  site_codecommit_repo_name = "${var.codecommit_repo_name != "" ? var.codecommit_repo_name : var.site_tld}"
+  site_tld_shortname        = "${replace(var.site_tld, ".", "")}"
 }
 
 # S3 bucket for website, public hosting
 resource "aws_s3_bucket" "main_site" {
-    bucket = "${var.site_tld}"
-    region = "${var.site_region}"
-    policy = <<EOF
+  bucket = "${var.site_tld}"
+  region = "${var.site_region}"
+
+  policy = <<EOF
 {
   "Id": "bucket_policy_site",
   "Version": "2012-10-17",
@@ -46,18 +47,20 @@ resource "aws_s3_bucket" "main_site" {
   ]
 }
 EOF
-    website {
-        index_document = "${var.root_page_object}"
-        error_document = "${var.error_page_object}"
-    }
-    # tags {
-    # }
-    # force_destroy = true
+
+  website {
+    index_document = "${var.root_page_object}"
+    error_document = "${var.error_page_object}"
+  }
+
+  # tags {
+  # }
+  # force_destroy = true
 }
 
 # S3 bucket for www redirect (optional)
 resource "aws_s3_bucket" "site_www_redirect" {
-  count = "${var.create_www_redirect_bucket == "true" ? 1 : 0}"
+  count  = "${var.create_www_redirect_bucket == "true" ? 1 : 0}"
   bucket = "www.${var.site_tld}"
   region = "${var.site_region}"
   acl    = "private"
@@ -94,10 +97,10 @@ resource "aws_s3_bucket" "site_cloudfront_logs" {
 
 # CodeCommit repo (optional)
 resource "aws_codecommit_repository" "codecommit_site_repo" {
-  count = "${var.create_codecommit_repo == "true" ? 1 : 0}"
+  count           = "${var.create_codecommit_repo == "true" ? 1 : 0}"
   repository_name = "${local.site_codecommit_repo_name}"
-  description = "This is the default repo for ${var.site_tld}"
-  default_branch = "master"
+  description     = "This is the default repo for ${var.site_tld}"
+  default_branch  = "master"
 }
 
 resource "aws_codecommit_trigger" "codecommit_notifications" {
@@ -184,15 +187,15 @@ EOF
 }
 
 resource "aws_kms_key" "codepipeline_kms_key" {
-  count = "${var.codepipeline_kms_key_arn == "" ? 1 : 0}"
-  description  = "KMS key to encrypt CodePipeline and S3 artifact bucket at rest for ${var.site_tld}"
+  count                   = "${var.codepipeline_kms_key_arn == "" ? 1 : 0}"
+  description             = "KMS key to encrypt CodePipeline and S3 artifact bucket at rest for ${var.site_tld}"
   deletion_window_in_days = 30
-  enable_key_rotation = "true"
+  enable_key_rotation     = "true"
 }
 
 resource "aws_kms_alias" "codepipeline_kms_key_name" {
-  count = "${var.codepipeline_kms_key_arn == "" ? 1 : 0}"
-  name = "alias/codepipeline-${local.site_tld_shortname}"
+  count         = "${var.codepipeline_kms_key_arn == "" ? 1 : 0}"
+  name          = "alias/codepipeline-${local.site_tld_shortname}"
   target_key_id = "${aws_kms_key.codepipeline_kms_key.key_id}"
 }
 
@@ -288,7 +291,7 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       "Action": [
         "cloudfront:CreateInvalidation"
       ],
-      "Resource": "${aws_cloudfront_distribution.site_cloudfront_distribution.arn}",
+      "Resource": "*",
       "Effect": "Allow"
     }
   ]
@@ -347,7 +350,7 @@ resource "aws_codebuild_project" "test_project" {
 # CodePipeline for deployment from CodeCommit to public site
 # Stages are configured in the CodePipeline object below. Add stages and referring CodeBuild projects above as necessary. Note that by default, the test stage is commented out, today.
 resource "aws_codepipeline" "site_codepipeline" {
-  name = "${var.site_tld}-codepipeline-provisioner"
+  name     = "${var.site_tld}-codepipeline-provisioner"
   role_arn = "${aws_iam_role.codepipeline_iam_role.arn}"
 
   artifact_store {
@@ -373,10 +376,11 @@ resource "aws_codepipeline" "site_codepipeline" {
 
       configuration {
         RepositoryName = "${local.site_codecommit_repo_name}"
-        BranchName = "master"
+        BranchName     = "master"
       }
     }
   }
+
   stage {
     name = "Test"
 
@@ -399,13 +403,13 @@ resource "aws_codepipeline" "site_codepipeline" {
     name = "Build"
 
     action {
-      name            = "Build"
-      category        = "Build"
-      owner           = "AWS"
-      provider        = "CodeBuild"
-      input_artifacts = ["${local.site_tld_shortname}-tested"]
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["${local.site_tld_shortname}-tested"]
       output_artifacts = ["${local.site_tld_shortname}-build"]
-      version         = "1"
+      version          = "1"
 
       configuration {
         ProjectName = "${aws_codebuild_project.build_project.name}"
@@ -418,7 +422,7 @@ resource "aws_codepipeline" "site_codepipeline" {
 resource "aws_cloudfront_distribution" "site_cloudfront_distribution" {
   origin {
     domain_name = "${aws_s3_bucket.main_site.website_endpoint}"
-    origin_id = "origin-bucket-${var.site_tld}"
+    origin_id   = "origin-bucket-${var.site_tld}"
 
     custom_origin_config {
       origin_protocol_policy = "http-only"
@@ -426,44 +430,51 @@ resource "aws_cloudfront_distribution" "site_cloudfront_distribution" {
       https_port             = "443"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
-    
+
     custom_header {
       name  = "User-Agent"
       value = "${var.site_secret}"
     }
   }
+
   logging_config = {
     include_cookies = "${var.log_include_cookies}"
-    bucket = "${aws_s3_bucket.site_cloudfront_logs.bucket_domain_name}"
-    prefix = "${local.site_tld_shortname}-"
+    bucket          = "${aws_s3_bucket.site_cloudfront_logs.bucket_domain_name}"
+    prefix          = "${local.site_tld_shortname}-"
   }
 
-  enabled = true
+  enabled             = true
   default_root_object = "${var.root_page_object}"
-  aliases = ["${var.site_tld}", "www.${var.site_tld}"]
-  price_class = "${var.cloudfront_price_class}"
-  retain_on_delete = true
+  aliases             = ["${var.site_tld}", "www.${var.site_tld}"]
+  price_class         = "${var.cloudfront_price_class}"
+  retain_on_delete    = true
+
   default_cache_behavior {
-    allowed_methods = [ "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT" ]
-    cached_methods = [ "GET", "HEAD" ]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = "origin-bucket-${var.site_tld}"
+
     forwarded_values {
       query_string = true
+
       cookies {
         forward = "none"
       }
     }
+
     viewer_protocol_policy = "redirect-to-https"
-    compress = true
-    min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
+    compress               = true
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
+
   viewer_certificate {
-    acm_certificate_arn = "${var.acm_site_certificate_arn}"
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = "${var.acm_site_certificate_arn}"
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -474,7 +485,8 @@ resource "aws_cloudfront_distribution" "site_cloudfront_distribution" {
 # SNS to support notifications for commit and build events
 resource "aws_sns_topic" "sns_topic" {
   count = "${var.create_sns_topic == "true" ? 1 : 0}"
-  name = "${var.sns_topic_name}"
+  name  = "${var.sns_topic_name}"
+
   # kms_master_key_id = "alias/codepipeline-${local.site_tld_shortname}"
 }
 
@@ -528,8 +540,8 @@ data "aws_iam_policy_document" "sns_topic_policy" {
 # DNS entry pointing to public site - optional
 
 resource "aws_route53_zone" "primary_site_tld" {
-  count = "${var.create_public_dns_zone == "true" ? 1 : 0}"  
-  name = "${var.site_tld}"
+  count = "${var.create_public_dns_zone == "true" ? 1 : 0}"
+  name  = "${var.site_tld}"
 }
 
 data "aws_route53_zone" "site_tld_selected" {
@@ -537,24 +549,24 @@ data "aws_route53_zone" "site_tld_selected" {
 }
 
 resource "aws_route53_record" "site_tld_record" {
-  count = "${var.create_public_dns_site_record == "true" ? 1 : 0}"
+  count   = "${var.create_public_dns_site_record == "true" ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.site_tld_selected.zone_id}"
-  name = "${var.site_tld}."
-  type = "A"
+  name    = "${var.site_tld}."
+  type    = "A"
 
   alias {
-    name = "${aws_cloudfront_distribution.site_cloudfront_distribution.domain_name}"
-    zone_id = "${aws_cloudfront_distribution.site_cloudfront_distribution.hosted_zone_id}"
+    name                   = "${aws_cloudfront_distribution.site_cloudfront_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.site_cloudfront_distribution.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
 
 resource "aws_route53_record" "site_www_record" {
-  count = "${var.create_public_dns_www_record == "true" ? 1 : 0}"
+  count   = "${var.create_public_dns_www_record == "true" ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.site_tld_selected.zone_id}"
-  name = "www"
-  type = "CNAME"
-  ttl = "5"
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "5"
 
-  records        = ["${var.site_tld}"]
+  records = ["${var.site_tld}"]
 }
