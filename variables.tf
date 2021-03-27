2caac02727447a90abbc5b7d9efd6142402b6e14
Jason Miller <jmiller@red-abstract.com>
2021-03-27 11:07:41 -0500

    Set version types

--- variables.tf
+++ variables.tf
@@ -1,75 +1,78 @@
 # Creation flags first
 
 variable "site_region" {
+  type        = string
   description = "Region in which to provision the site. Default: us-east-1"
   default     = "us-east-1"
 }
 
 variable "create_www_redirect_bucket" {
+  type        = bool
   description = "Defines whether or not to create a www redirect S3 bucket."
   default     = true
 }
 
 variable "content_bucket_versioning" {
+  type        = bool
   description = "Defines whether or not to set versioning on the content bucket."
   default     = true
 }
 
 variable "create_cloudfront_distribution" {
+  type        = bool
   description = "Defines whether or not to create a CloudFront distribution for the S3 bucket."
   default     = true
 }
 
 variable "log_include_cookies" {
+  type        = bool
   description = "Defines whether or not CloudFront should log cookies."
   default     = false
 }
 
-variable "create_sns_topic" {
-  description = "Defines whether or not to create an SNS topic for notifications about events."
-  default     = false
-}
-
-variable "sns_topic_name" {
-  description = "Name for the SNS topic."
-  default     = "website-notifications"
-}
-
 variable "site_tld" {
+  type        = string
   description = "TLD of the website you want to create. A bucket will be created that is named this. Note that the module will error out if this bucket already exists in AWS. Example: example.com"
 }
 
 variable "create_public_dns_zone" {
+  type        = bool
   description = "If set to true, creates a public hosted zone in Route53 for your site."
-  default     = "false"
+  default     = false
 }
 
 variable "create_public_dns_site_record" {
+  type        = bool
   description = "If set to true, creates a public DNS record in your site_tld hosted zone. If you do not already have a hosted zone for this TLD, you should set create_public_dns_zone to true. Otherwise, this will try to create a record in an existing zone or fail."
-  default     = "true"
+  default     = true
 }
 
 variable "create_public_dns_www_record" {
+  type        = bool
   description = "Defines whether or not to create a WWW DNS record for the site."
   default     = false
 }
 
 variable "root_page_object" {
+  type        = string
   description = "The root page object for the Cloudfront/S3 distribution."
   default     = "index.html"
 }
 
 variable "error_page_object" {
+  type        = string
   description = "The error page object for the Cloudfront/S3 distribution."
   default     = "404.html"
 }
 
 variable "cloudfront_price_class" {
+  type        = string
   description = "Price class for Cloudfront."
   default     = "PriceClass_100"
 }
 
 variable "acm_site_certificate_arn" {
+  type        = string
   description = "ARN of an ACM certificate to use for https on the CloudFront distribution."
 }
 
