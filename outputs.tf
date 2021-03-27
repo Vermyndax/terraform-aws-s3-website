8ced000c39869ea6db32db05572217898f875ce6
Jason Miller <jmiller@red-abstract.com>
2021-03-27 11:01:26 -0500

    Proper conditional on output

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,10 @@
 output "content_sync_access_key" {
-  count       = var.create_content_sync_user == true ? 1 : 0
-  value       = aws_iam_access_key.content_sync_key[0].id
+  value       = var.create_content_sync_user ? aws_iam_access_key.content_sync_key[0].id : ""
   description = "Access key ID of the optional content sync user."
 }
 
 output "content_sync_access_secret" {
-  count       = var.create_content_sync_user == true ? 1 : 0
-  value       = aws_iam_access_key.content_sync_key[0].secret
+  value       = var.create_content_sync_user ? aws_iam_access_key.content_sync_key[0].secret : ""
   sensitive   = true
   description = "Secret Access key of the optional content sync user. This is marked as sensitive and will not show in plan output, but be aware that it is stored in your state file. Encrypt accordingly."
 }
